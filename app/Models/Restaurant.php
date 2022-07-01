666af4dabd1bc6883c38d8773af8eee442213067
agahebeye <agahebeye@gmail.com>
2022-07-01 17:36:03 +0200

    add restaurant model relationships

--- app/Models/Restaurant.php
+++ app/Models/Restaurant.php
@@ -2,10 +2,29 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Restaurant extends Model
 {
     use HasFactory;
+
+    public $timestamps = false;
+
+    protected $fillable = ['name', 'slug', 'description', 'founded_at'];
+
+    protected $casts = [
+        'founded_at' => 'datetime'
+    ];
+
+    protected static function booted()
+    {
+        static::creating(fn ($model) => $model->slug = Str::slug($model->name));
+    }
+
+    public function owners()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
